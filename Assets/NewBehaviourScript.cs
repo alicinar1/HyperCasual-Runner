4f39fd8b85f5465b83e59caf8c1d248a306bf1fe
alicinar1 <muhammeda.cinar@gmail.com>
2022-01-29 19:05:06 +0300

    Importing assets

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+// tap and hold to speed up
+// collect energy
+// at the end of the game according to your energy jump 
+// obstacles
+// maybe jump
+// energy powerups 
+// maybe health
+// score is at the end of the game at the jumping part jump forward higher score
+}
